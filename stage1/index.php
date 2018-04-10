f61481f01c01b4af19fa0fd3e566e3866e8157bf
Samuel <wekesamuel@yahoo.com>
2018-04-10 23:45:36 +0100

    asa

---
+++ stage1/index.php
@@ -0,0 +1,16 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Stage 1 Task</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+</head>
+<body>
+	<div class="container">
+		<h1>WELCOME <br>TO THE WORLD OF <br>PROGRAMMING</h1>
+
+		<div class="time"><?php echo date("F j, Y, g:i a"); ?></div>
+	</div>
+
+</body>
+</html>
